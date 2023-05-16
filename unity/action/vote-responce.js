91f188ea39e884cf6dd6f4105f5202e9b2e81b8c
goldball1105 <116549839+goldball1105@users.noreply.github.com>
2023-05-16 16:26:02 +0800

    vote system done

--- unity/action/vote-responce.js
+++ unity/action/vote-responce.js
@@ -6,27 +6,24 @@ module.exports = {
         const name = interaction.fields.getTextInputValue('vote-title');
         const about = interaction.fields.getTextInputValue('vote-des');
 
-        let upvotec = 0;
-        let downvotec = 0;
-
         const embed = new EmbedBuilder()
             .setTitle(`${name}`)
             .setDescription(`${about}\n`)
-            .addFields(
-                { name: `———<:upvote:1051138113168212029>———`, value: `人數：\`${upvotec}\``, inline: true },
-                { name: `———<:downvote:1051138166096154674>———`, value: `人數：\`${downvotec}\``, inline: true }
-            )
+            .addFields([
+                { name: `—<:upvote:1051138113168212029> 人數`, value: `0`, inline: true },
+                { name: `—<:downvote:1051138166096154674> 人數`, value: `0`, inline: true }
+            ])
             .setTimestamp()
 
         const replything = await interaction.reply({ embeds: [embed], fetchReply: true });
 
         const upvote = new ButtonBuilder()
-            .setCustomId(`upvote-${replything.id}`)
+            .setCustomId(`vote-up-${replything.id}`)
             .setLabel('贊成')
             .setStyle(ButtonStyle.Success)
 
         const downvote = new ButtonBuilder()
-            .setCustomId(`downvote-${replything.id}`)
+            .setCustomId(`vote-down-${replything.id}`)
             .setLabel('反對')
             .setStyle(ButtonStyle.Danger)
 
@@ -34,7 +31,6 @@ module.exports = {
             .addComponents(upvote, downvote)
 
         await interaction.editReply({ components: [row] })
-//=============================
         
     }
 };
