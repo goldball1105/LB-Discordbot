8ddefb7db0cbda5386ea3f452bc6f676f5e552db
goldball1105 <116549839+goldball1105@users.noreply.github.com>
2023-05-17 11:01:47 +0800

    whitelist system ing

---
+++ unity/action/whitelist.js
@@ -0,0 +1,36 @@
+const { EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } = require('discord.js')
+
+module.exports = {
+    customId: 'vote',
+    async execute(interaction, client) {
+        const name = interaction.fields.getTextInputValue('vote-title');
+        const about = interaction.fields.getTextInputValue('vote-des');
+
+        const embed = new EmbedBuilder()
+            .setTitle(`${name}`)
+            .setDescription(`${about}\n`)
+            .addFields(
+                { name: `———<:upvote:1051138113168212029>———`, value: `人數：\`0\``, inline: true },
+                { name: `———<:downvote:1051138166096154674>———`, value: `人數：\`0\``, inline: true }
+            )
+            .setTimestamp()
+
+        const replything = await interaction.reply({ embeds: [embed], fetchReply: true });
+
+        const upvote = new ButtonBuilder()
+            .setCustomId(`vote-up-${replything.id}`)
+            .setLabel('贊成')
+            .setStyle(ButtonStyle.Success)
+
+        const downvote = new ButtonBuilder()
+            .setCustomId(`vote-down-${replything.id}`)
+            .setLabel('反對')
+            .setStyle(ButtonStyle.Danger)
+
+        const row = new ActionRowBuilder()
+            .addComponents(upvote, downvote)
+
+        await interaction.editReply({ components: [row] })
+
+    }
+};
--- unity/whitelist.js
+++ unity/whitelist.js
@@ -1,35 +1,35 @@
 const { EmbedBuilder, ActionRowBuilder, ButtonBuilder } = require('@discordjs/builders');
-const { VerifyChannel, RuleChannel, emojiverify, verifyMsgId } = require('../config.json');
+const { whitelistChannel, emojiwhitelist, whitelistMsgId } = require('../config.json');
 const { Message } = require('discord.js');
 
 module.exports = {
     name: 'ready',
     async execute(interaction, client) {
-        const channel = interaction.channels.cache.get(VerifyChannel);
+        const channel = interaction.channels.cache.get(whitelistChannel);
 
         const embed = new EmbedBuilder()
             .setColor(0x67c773)
-            .setTitle('驗證訊息')
-            .setDescription(`> 在按下 **驗證** 按鈕前請先閱讀<#${RuleChannel}>\n> 在進行動作喔`);
+            .setTitle('閱讀完以上規則了')
+            .setDescription(`> 現在你可以按下以下按鈕開始遊玩了！\n> **一定要閱讀完規則**\n> **一定要閱讀完規則**\n> **一定要閱讀完規則**\n因為很重要所以要說三次`);
 
         const button = new ButtonBuilder()
-            .setCustomId('veritfy')
-            .setLabel('驗證') 
-            .setEmoji({ name: emojiverify })
+            .setCustomId('wlform')
+            .setLabel('開始遊玩')
+            .setEmoji({ name: emojiwhitelist })
             .setStyle('Success');
 
         const row = new ActionRowBuilder()
             .addComponents(button);
 
         try {
-            const msg = await channel.messages?.fetch(verifyMsgId).catch(() => { });
-            if (msg) { 
-                if (msg.embeds[0].title === embed.title) { 
+            const msg = await channel.messages?.fetch(whitelistMsgId).catch(() => { });
+            if (msg) {
+                if (msg.embeds[0].title === embed.title) {
                     await msg.edit({ embeds: [embed], components: [row] });
                 } else {
                     await msg.edit({ embeds: [embed], components: [row] });
                 }
-            } else { 
+            } else {
                 await channel.send({ embeds: [embed], components: [row] });
             }
         } catch (e) {
