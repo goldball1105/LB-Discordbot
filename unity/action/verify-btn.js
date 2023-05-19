e1fd4aecb9924669ef58c42725731a785fb97ae6
goldball1105 <goldballminecraft@gmail.com>
2023-05-19 12:24:08 +0800

    ui add

--- unity/action/verify-btn.js
+++ unity/action/verify-btn.js
@@ -1,15 +1,15 @@
 const { EmbedBuilder } = require('discord.js')
-const { verifyRoleone, verifyRoletwo } = require('../../config.json')
+const { verifyRoleone, verifyRoletwo, VerifylogChannel } = require('../../config.json')
 module.exports = {
     customId: 'veritfy',
     async execute(interaction, client){
         const embedsuccess = new EmbedBuilder()
         .setTitle('成功驗證')
-        .setDescription('> \< 你以成功驗證身分 \>\n> 接下來你可以到以下頻道獲取相關知識')
+        .setDescription('> **你以成功驗證身分**\n> 接下來你可以到以下頻道獲取相關知識')
         .setFields(
-            { name: '-------', value:'> 獲取相關身分主\n> 回報問題我們\n> 遊戲同步\n> 閒聊吧\n> Moordi建議我plz', inline: true },
-            { name:'\u200B', value:`<#1092446938630193273>\n<#1069949269265621033>\n<#1092423289651081236>\n<#1092673963387260958>\n<@${735452040381333535}>`, inline: true },
-            { name:'請等待10秒', value:`秒後機器人將自動給你身分主觀看更多頻道` }
+            { name: '\u200B', value:'> 獲取相關身分主\n> 回報問題我們\n> 遊戲同步\n> 閒聊吧', inline: true },
+            { name:'\u200B', value:`<#1092446938630193273>\n<#1069949269265621033>\n<#1092423289651081236>\n<#1098961853020913725>`, inline: true },
+            { name:'請等待 `10` 秒', value:`秒後機器人將自動給你身分組觀看更多頻道` }
         )
         const embedfail = new EmbedBuilder()
         .setTitle('成功驗證')
@@ -27,6 +27,14 @@ module.exports = {
           } else {
             
             await interaction.reply({ embeds: [embedsuccess], ephemeral: true,});
+
+            const log = new EmbedBuilder()
+                .setColor(0xeb4034)
+                .setTitle('使用者驗證')
+                .setDescription(`驗證者：${interaction.user}\ntag：${interaction.user.tag}\n使用者Id：\`${interaction.user.id}\``)
+
+            const channel = interaction.guild.channels.cache.get(VerifylogChannel);
+            channel.send({ embeds: [log] })
 
             setTimeout(() => {
                 interaction.member.roles.add(verifyRoleone);
