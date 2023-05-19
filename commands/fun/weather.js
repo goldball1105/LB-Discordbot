2b2d3a66cf2911009dda02434d5c00ce9e5e686b
goldball1105 <goldballminecraft@gmail.com>
2023-05-19 14:25:34 +0800

    weather

--- commands/fun/weather.js
+++ commands/fun/weather.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder } = require('discord.js');
-const tw = require('taiwan-weather');
+const weather = require('taiwan-weather');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -7,10 +7,16 @@ module.exports = {
         .setDescription('天氣再台灣'),
 
     async execute(interaction) {
-        tw.get('CWB-ABF2CCEB-E6ED-4F5C-81BE-9567D373C48F', null, err => {
-            interaction.reply('rr')
-        })
+        let data = weather.get('CWB-ABF2CCEB-E6ED-4F5C-81BE-9567D373C48F', {
+            loc: [tw.DataEnum.Loc.TAIPEI_CITY, tw.DataEnum.Loc.HSINCHU_CITY],
+            freq: tw.DataEnum.Freq.WEEKDAY,
+            lang: tw.DataEnum.Lang.EN,
+            output: 'data',
+            prefix: Date.now() + '_',
+            toJson: true,
+            debug: true },
+        )
 
-
+        console.log(data);
     },
 };
