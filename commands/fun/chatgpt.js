a1b68d0f026e7cf0b984f0fc1271e9190c9e28df
goldball1105 <goldballminecraft@gmail.com>
2023-05-19 15:16:32 +0800

    gpt thing

--- commands/fun/chatgpt.js
+++ commands/fun/chatgpt.js
@@ -28,7 +28,7 @@ module.exports = {
                 model: 'text-davinci-003',
                 max_tokens: 2048,
                 temperature: 0.5,
-                prompt: question
+                prompt: question + "#zh_tw"
             })
 
             const embed = new EmbedBuilder()
