0d01016b8aa2f8e2538229d7c79bfc5a90034e7b
shane04111 <57738232+shane04111@users.noreply.github.com>
2023-05-24 05:37:38 +0800

    Add files via upload

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -36,12 +36,15 @@ module.exports = {
 
 		if (command) {
 			try {
-				await command.execute(interaction);
+				if (interaction.isAutocomplete()) {
+					await command.autocomplete(interaction);
+				} else {
+					await command.execute(interaction);
+				}
 			} catch (error) {
 				console.error(`Error executing ${interaction.commandName}`);
 				console.error(error);
 			}
 		}
-
 	},
 };
