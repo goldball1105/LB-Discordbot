f7741fe778cee23b57d18b2bd30d28e4f5b56e04
goldball1105 <goldballminecraft@gmail.com>
2023-05-24 15:57:06 +0800

    chalk

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ for (const folder of commandFolders) {
 		const command = require(filePath);
 		if ('data' in command && 'execute' in command) {
 			client.commands.set(command.data.name, command);
+			console.log(`成功載入 指令:[${file}]`)
 		} else {
 			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 		}
